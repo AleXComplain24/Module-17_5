b8305065d4ea1093403f84dacbcd37d8ec961f32
AleXComplain24 <alexkorg98@gmail.com>
2024-12-21 12:18:57 +0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -50,7 +50,8 @@ class CreateTask(BaseModel):
     title: str
     content: str
     priority: int
-
+    user_id: int
+    
 class UpdateTask(BaseModel):
     title: str = None
     content: str = None
